905c1edad764d24406a13c4005d891d5d41112ba
valentina cervo <valentinacervo883@gmail.com>
2025-10-15 09:25:01 -0300

    versao com saudação

---
+++ nodejs/saudacao-mvc/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Middleware para arquivos estáticos
+app.use(express.static('public'));
+
+// Middleware para ler dados de formulários
+app.use(express.urlencoded({ extended: true }));
+
+// Rotas
+const indexRoutes = require('./routes/index');
+app.use('/', indexRoutes);
+
+// Rotas
+const usersRoutes = require('./routes/users');
+app.use('/users', usersRoutes);
+
+const escolaRoutes = require('./routes/escola');
+app.use('/escola', escolaRoutes);
+
+// Servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
+
---
+++ nodejs/saudacao-mvc/models/saudacaoModel.js
@@ -0,0 +1,18 @@
+module.exports = {
+  gerarMensagemPersonalizada: (nome, idade, localizacao) => {
+    let faixaEtaria;
+
+    if (idade < 12) {
+      faixaEtaria = "criança";
+    } else if (idade < 18) {
+      faixaEtaria = "adolescente";
+    } else if (idade < 60) {
+      faixaEtaria = "adulto";
+    } else {
+      faixaEtaria = "idoso";
+    }
+
+    return `Olá, ${nome}! Você é um(a) ${faixaEtaria} e mora em ${localizacao}.`;
+  }
+};
+
